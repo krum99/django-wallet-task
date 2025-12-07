83d64b003e9a46c7df79e7596fe0c536be2d1365
krum99 <79230210+krum99@users.noreply.github.com>
2025-12-07 11:30:33 +0200

    feat: implement register and login

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
 
-# Create your views here.
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+
+def home_view(request):
+    return render(request, 'accounts/home.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'accounts/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'accounts/register.html', {'form': form})
